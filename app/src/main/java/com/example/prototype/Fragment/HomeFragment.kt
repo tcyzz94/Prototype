60c26e3051d77425d865c7e7ffc251adaf522de2
Ting Chuan Yuan <ahting22289@hotmail.com>
2020-04-08 18:24:58 +0800

    first try tho

---
+++ app/src/main/java/com/example/prototype/Fragment/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.example.prototype.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.prototype.R
+
+class HomeFragment : Fragment() {
+    private lateinit var ivTimeTable: ImageView
+    private lateinit var ivAddStudent: ImageView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        ivAddStudent = view.findViewById(R.id.iv_add_std)
+        ivTimeTable = view.findViewById(R.id.iv_time_table)
+
+        ivTimeTable.setOnClickListener {
+            findNavController().navigate(R.id.timeTableFragment,null)
+        }
+    }
+}
--- app/src/main/java/com/example/prototype/MainActivity.kt
+++ app/src/main/java/com/example/prototype/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.prototype
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
+
+        val navController = host.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.my_nav_host_fragment).navigateUp(appBarConfiguration)
+    }
+
+
 }
